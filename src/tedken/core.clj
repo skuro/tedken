4db0beb8adc0985cd65f8c4731b6fd3834f98282
Carlo Sciolla <skuro@skuro.tk>
2015-05-12 08:33:38 +0200

    Initial refactoring for proper code / test separation

--- src/tedken/core.clj
+++ src/tedken/core.clj
@@ -1,6 +1,48 @@
-(ns tedken.core)
+(ns tedken.core
+  (:require [environ.core :refer [env]]
+            [lock-key.core :as sec]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn valid?
+  "Decrypts the token and validate its dough"
+  [token]
+  {:pre  [(string? token)]}
+  (not (nil? token)))
+
+(defn secured?
+  "Checks if the request contain a valid CSRF protection token"
+  [request]
+  (if-let [token (get-in request [:headers "X-CSRF-Token"])]
+    (valid? token)
+    false))
+
+(def encoder (java.util.Base64/getEncoder))
+
+(def decoder (java.util.Base64/getDecoder))
+
+(defn secure
+  "Encrypts a string using AES"
+  [s]
+  (-> s
+      (sec/encrypt (env :tedken-key))
+      (->> (.encodeToString encoder))))
+
+(defn decode
+  "Decodes a Base64 string into bytes"
+  [encrypted]
+  (.decode decoder encrypted))
+
+(defn decrypt
+  "Decrypts the token"
+  [token]
+  (let [decoded (decode token)]
+    (String. (sec/decrypt decoded (env :tedken-key)) "UTF-8")))
+
+(defn wrap-csrf-token
+  "RING wrapper that creates and sets encrypted tokens to prevent CSRF attacks"
+  [handler]
+  (fn [request]
+    (if (secured? request)
+      (handler request)
+      {:status 401
+       :headers {}
+       :body "You shall not pass!"})))
--- test/tedken/core_test.clj
+++ test/tedken/core_test.clj
@@ -1,8 +1,6 @@
 (ns tedken.core-test
   (:use [midje.sweet])
-  (:require [tedken.core :refer :all]
-            [environ.core :refer [env]]
-            [lock-key.core :refer [decrypt decrypt-as-str encrypt]]))
+  (:require [environ.core :refer [env]]))
 
 (defn insecure-request
   "Creates a RING request with no token"
@@ -23,29 +21,6 @@
    :headers {}
    :body "Everything is ok!"})
 
-(defn valid?
-  "Decrypts the token and validate its dough"
-  [token]
-  {:pre  [(string? token)]}
-  (not (nil? token)))
-
-(defn secured?
-  "Checks if the request contain a valid CSRF protection token"
-  [request]
-  (if-let [token (get-in request [:headers "X-CSRF-Token"])]
-    (valid? token)
-    false))
-
-(defn wrap-csrf-token
-  "RING wrapper that creates and sets encrypted tokens to prevent CSRF attacks"
-  [handler]
-  (fn [request]
-    (if (secured? request)
-      (handler request)
-      {:status 401
-       :headers {}
-       :body "You shall not pass!"})))
-
 (def secured-handler (wrap-csrf-token dummy-handler))
 
 (let [secured (secure-request "foobar")
@@ -55,28 +30,6 @@
                (secured-handler insecure)    => (contains #{[:status 401]}))
          (fact "Secure requests are processed"
                (secured-handler secured)    => (contains #{[:status 200]}))))
-
-(def encoder (java.util.Base64/getEncoder))
-
-(def decoder (java.util.Base64/getDecoder))
-
-(defn secure
-  "Encrypts a string using AES"
-  [s]
-  (-> s
-      (encrypt (env :tedken-key))
-      (->> (.encodeToString encoder))))
-
-(defn decode
-  "Decodes a Base64 string into bytes"
-  [encrypted]
-  (.decode decoder encrypted))
-
-(defn unwrap
-  "Decrypts the token"
-  [token]
-  (let [decoded (decode token)]
-    (String. (decrypt decoded (env :tedken-key)) "UTF-8")))
 
 (let [secret "secret"]
   (fact "I can encrypt the token"
