64d7d0f3ea86daf6228d53041b99df8be913173d
Carlo Sciolla <skuro@skuro.tk>
2015-05-13 17:58:59 +0200

    new deps

--- project.clj
+++ project.clj
@@ -7,8 +7,8 @@
                  [lock-key "1.1.0"]
                  [org.clojure/data.codec "0.1.0"]
                  [crypto-random "1.2.0"]
-                 [simple-time "0.2.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]
-                                  [environ "1.0.0"]]
+                 [simple-time "0.2.0"]
+                 [environ "1.0.0"]]
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]
                    :plugins [[lein-midje "3.1.3"]]
                    :env {:tedken.key "secret"}}})
